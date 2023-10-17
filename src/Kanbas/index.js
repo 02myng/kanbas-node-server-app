a498886e890b3ff23dae10e6a88a71b1e836725b
Eileen Lee <lee.ei@northeastern.edu>
2023-10-17 15:14:43 -0400

    a3 lab

---
+++ src/Kanbas/index.js
@@ -0,0 +1,15 @@
+import KanbasNavigation from "./KanbasNavigation.js";
+
+function Kanbas() {
+  return (
+    <div className="d-flex">
+      <KanbasNavigation />
+      <div>
+        <h1>Account</h1>
+        <h1>Dashboard</h1>
+        <h1>Courses</h1>
+      </div>
+    </div>
+  );
+}
+export default Kanbas;
